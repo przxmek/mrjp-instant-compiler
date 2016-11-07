ae44c464bdafbf6912ff2c14028d21e98173ea40
Przemek Kuczynski <przemek@pkuczynski.pl>
2016-11-07 13:24:00 +0100

    JVM fixes and refactor

--- src/CompilerJVM.hs
+++ src/CompilerJVM.hs
@@ -6,26 +6,37 @@ import Data.Map as Map
 import Parser.AbsInstant
 
 
+
+
 type Result     = StateT Env IO (StackSize, ProgramTxt)
-type Env        = Map Ident Val
-type Val        = Int
+type Env        = Map Ident Location
+type Location   = Int
 type StackSize  = Int
 type ProgramTxt = String
 
-data Instr = BinInstr
-data BinInstr = IAdd | ISub | IMul | IDiv deriving (Eq, Ord, Show, Read)
--- data BinaryOp = OpAdd | OpSub | OpMul | OpDiv deriving (Eq, Ord, Show, Read)
+data Instr 
+    = IConstM1 | IConst_ Integer | BiPush Integer | SiPush Integer | Ldc Integer 
+    | ILoad Location | IStore Location
+    | BiI BinaryInstr 
+    | PrintStreamISwap
+  deriving (Eq, Ord, Show, Read)
+
+data BinaryInstr = IAdd | ISub | IMul | IDiv 
+  deriving (Eq, Ord, Show, Read)
 
 
-emptyProgram :: (Int, String)
-emptyProgram = (0, "")
+
+
+emptyResult :: (Int, String)
+emptyResult = (0, "")
 
 
 compile :: Program -> IO String
 compile program = do
   ((stack, mainTxt), env) <- runStateT (transProgram program) Map.empty
-  let locals = (+1) $ Map.size env
-  let name = "Program" in return (
+  let locals = Map.size env
+  let name = "Program"
+  return (
     ".class  public " ++ name ++ "\n" ++
     ".super  java/lang/Object\n\n" ++
     ".method public <init>()V\n" ++
@@ -43,13 +54,13 @@ transProgram :: Program -> Result
 transProgram (Prog stmts) =  do
   results <- mapM transStmt stmts
   let programConcat (accStack, accTxt) (stack, txt) = (max accStack stack, accTxt ++ txt)
-  let (stack, text) = Prelude.foldl programConcat emptyProgram results
+  let (stack, text) = Prelude.foldl programConcat emptyResult results
   return (stack, text)
 
 
 transStmt :: Stmt -> Result
 transStmt (SAss ident expr)  = do
-  (exprSt, exprTxt) <- transExp expr
+  (exprStack, exprTxt) <- transExp expr
   env <- get
   loc <- case Map.lookup ident env of
     Just loc -> return loc
@@ -57,71 +68,67 @@ transStmt (SAss ident expr)  = do
       let loc = Map.size env
       put $ Map.insert ident loc env 
       return loc
-  return (exprSt, exprTxt ++ "  istore " ++ show loc ++ "\n")
+  (_, assTxt) <- jvmInstr $ IStore loc
+
+  return (exprStack, exprTxt ++ assTxt)
 
 
 transStmt (SExp expr)        = do
-  (stack, txt) <- transExp expr
-  return (stack + 1, txt ++ jvmPrint) where 
-    jvmPrint = "  getstatic java/lang/System/out Ljava/io/PrintStream;\n" ++
-               "  swap\n" ++
-               "  invokevirtual java/io/PrintStream/println(I)V\n"
+  (stack, txt)  <- transExp expr
+  (printStack, printTxt) <- jvmInstr PrintStreamISwap
+  return (stack + printStack, txt ++ printTxt)
 
 
 transExp :: Exp -> Result
-transExp (ExpAdd exp1 exp2)  = transBinaryOp IAdd exp1 exp2
-transExp (ExpSub exp1 exp2)  = transBinaryOp ISub exp1 exp2
-transExp (ExpMul exp1 exp2)  = transBinaryOp IMul exp1 exp2
-transExp (ExpDiv exp1 exp2)  = transBinaryOp IDiv exp1 exp2
+transExp (ExpAdd exp1 exp2)  = transBinaryOp (BiI IAdd) exp1 exp2
+transExp (ExpSub exp1 exp2)  = transBinaryOp (BiI ISub) exp1 exp2
+transExp (ExpMul exp1 exp2)  = transBinaryOp (BiI IMul) exp1 exp2
+transExp (ExpDiv exp1 exp2)  = transBinaryOp (BiI IDiv) exp1 exp2
 transExp (ExpLit num)        = transIConst num
 transExp (ExpVar ident)      = do
   env <- get
   let loc = env Map.! ident
-  return (1, "  iload " ++ show loc ++ "\n")
+  jvmInstr $ ILoad loc
 
 
-
-transBinaryOp :: BinInstr -> Exp -> Exp -> Result
-transBinaryOp op exp1 exp2  = do
+transBinaryOp :: Instr -> Exp -> Exp -> Result
+transBinaryOp op@(BiI _) exp1 exp2  = do
   (e1St, e1Txt) <- transExp exp1
   (e2St, e2Txt) <- transExp exp2
   (   _, opTxt) <- jvmInstr op
-  -- let rSt = max e1St e2St
   return (e1St + e2St, e1Txt ++ e2Txt ++ opTxt)
+transBinaryOp _ _ _ = return emptyResult
 
 
 transIConst :: Integer -> Result
 transIConst num 
-  | num ==     -1                 = wrapInstr1 "iconst_m1"
-  | num >=      0 && num <     6  = wrapInstr1 $ "iconst_" ++ show num
-  | num >=   -128 && num <   128  = wrapInstr1 $ "bipush " ++ show num
-  | num >= -32768 && num < 32768  = wrapInstr1 $ "sipush " ++ show num
-  | otherwise                     = wrapInstr1 $ "ldc "    ++ show num
+  | num ==     -1                 = jvmInstr   IConstM1
+  | num >=      0 && num <     6  = jvmInstr $ IConst_ num
+  | num >=   -128 && num <   128  = jvmInstr $ BiPush num
+  | num >= -32768 && num < 32768  = jvmInstr $ SiPush num
+  | otherwise                     = jvmInstr $ Ldc num
 
 
 
 
-jvmInstr :: BinInstr -> Result
-jvmInstr IAdd = wrapInstr 0 "iadd"
-jvmInstr ISub = wrapInstr 0 "isub"
-jvmInstr IMul = wrapInstr 0 "imul"
-jvmInstr IDiv = wrapInstr 0 "idiv"
+jvmInstr :: Instr -> Result
+jvmInstr  (BiI IAdd) = return (0, formatInstr   "iadd")
+jvmInstr  (BiI ISub) = return (0, formatInstr   "isub")
+jvmInstr  (BiI IMul) = return (0, formatInstr   "imul")
+jvmInstr  (BiI IDiv) = return (0, formatInstr   "idiv")
+jvmInstr   IConstM1  = return (1, formatInstr   "iconst_m1")
+jvmInstr (IConst_ i) = return (1, formatInstr $ "iconst_" ++ show i)
+jvmInstr  (BiPush i) = return (1, formatInstr $ "bipush " ++ show i)
+jvmInstr  (SiPush i) = return (1, formatInstr $ "sipush " ++ show i)
+jvmInstr     (Ldc i) = return (1, formatInstr $    "ldc " ++ show i)
+jvmInstr   (ILoad l) = return (1, formatInstr $  "iload " ++ show l)
+jvmInstr  (IStore l) = return (1, formatInstr $  "istore " ++ show l)
+
+jvmInstr PrintStreamISwap = return
+  (1, "  getstatic java/lang/System/out Ljava/io/PrintStream;\n" ++
+      "  swap\n" ++
+      "  invokevirtual java/io/PrintStream/println(I)V\n")
 
 
--- op2Instr :: BinaryOp -> Result
--- op2Instr OpAdd  = wrapInstr0 "iadd"
--- op2Instr OpSub  = wrapInstr0 "isub"
--- op2Instr OpMul  = wrapInstr0 "imul"
--- op2Instr OpDiv  = wrapInstr0 "idiv"
-
-
-
-
-wrapInstr :: Int -> String -> Result
-wrapInstr s instr  = return (s, "  " ++ instr ++ "\n")
-
-wrapInstr0 :: String -> Result
-wrapInstr0 = wrapInstr 0
-
-wrapInstr1 :: String -> Result
-wrapInstr1  = wrapInstr 1
+formatInstr :: String -> String
+formatInstr   instr  = "  " ++ instr ++ "\n"
